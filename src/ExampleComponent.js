730ab6e9a912448a08cbdf9a2cf5cfbe7b35fcf9
Bryan Weber <bryan@Bryans-MacBook-Pro.local>
2022-02-08 17:59:10 -0800

    first commit

---
+++ src/ExampleComponent.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { useQuill } from 'react-quilljs';
+import BlotFormatter from 'quill-blot-formatter';
+import 'quill/dist/quill.snow.css';
+
+import './styles.css';
+
+const Editor = ({triggerQuery, model, modelUpdate}) => {
+  const {input, toolbar} = model;
+  let modules = { blotFormatter: {} }
+  if (toolbar) modules['toolbar'] = toolbar
+
+  const { quill, quillRef, Quill } = useQuill({
+    theme: 'snow',
+    modules
+  });
+  
+
+  if (Quill && !quill) {
+    // const BlotFormatter = require('quill-blot-formatter');
+    Quill.register('modules/blotFormatter', BlotFormatter);
+  }
+
+  useEffect(() => {
+    if (quill) {
+      quill.on('text-change', (delta, oldContents) => {
+        modelUpdate({output: quillRef.current.firstChild.innerHTML})
+      });
+    }
+  }, [quill, Quill]);
+
+  useEffect(()=>{
+    console.log(input)
+    if (input && quillRef.current && quillRef.current.firstChild && quill && quill.clipboard) {
+      if (input !== quillRef.current.firstChild.innerHTML) {
+        quill.clipboard.dangerouslyPasteHTML( input )
+        modelUpdate({output: quillRef.current.firstChild.innerHTML, input: quillRef.current.firstChild.innerHTML})
+      }
+    }
+  },[input, quillRef.current, quill])
+
+
+  return (
+    <div id={"quill"}>
+      <div ref={quillRef} />
+    </div>
+  );
+};
+
+export default Editor;
